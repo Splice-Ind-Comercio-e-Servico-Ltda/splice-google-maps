bc40d9c61ad33a3eb2bec2800058dddea6dab45f
Breno Augusto da Silva Moreno <breno_asm@hotmail.com>
2019-09-30 15:28:31 -0300

    The infowindow is rendering.

--- src/demo/App.js
+++ src/demo/App.js
@@ -4,26 +4,46 @@ import SpliceGoogleMaps from '../lib/index';
 
 import './app.css';
 
+const Info = () => <div>This is an info window!</div>;
+
 const App = () => {
   const [selectedMarkers, setSelectedMarkers] = useState([]);
   const markers = [
     {
       position: { lat: -19.0935, lng: -75.5984 },
+      infoWindow: {
+        content: Info,
+      },
     },
     {
       position: { lat: 55.45132, lng: 0.840424 },
+      infoWindow: {
+        content: Info,
+      },
     },
     {
       position: { lat: 0.840424, lng: 55.45132 },
+      infoWindow: {
+        content: Info,
+      },
     },
     {
       position: { lat: 55.45132, lng: 27.53625 },
+      infoWindow: {
+        content: Info,
+      },
     },
     {
       position: { lat: -19.0935, lng: -85.23243 },
+      infoWindow: {
+        content: Info,
+      },
     },
     {
       position: { lat: 55.45132, lng: -19.0935 },
+      infoWindow: {
+        content: Info,
+      },
     },
   ];
 
--- src/lib/components/markers.js
+++ src/lib/components/markers.js
@@ -1,11 +1,14 @@
-import { useContext, useState, useEffect, useCallback } from 'react';
+import React, { useContext, useState, useEffect, useCallback } from 'react';
+import { render } from 'react-dom';
 import PropTypes from 'prop-types';
 
 import { MapContext } from '../mapContext';
 
 import { markerPropTypes } from '../propTypes';
 
-import { hasContent } from '../utils';
+import { hasContent, setupGoogleMapsEventListeners, isAFunction } from '../utils';
+
+// import InfoWindow from './infoWindow';
 
 const propTypes = {
   markers: PropTypes.arrayOf(PropTypes.shape(markerPropTypes)),
@@ -20,15 +23,69 @@ const Markers = ({ markers }) => {
 
   const [_markers, _setMarkers] = useState([]);
 
+  const _renderToDom = useCallback((Content) => {
+    render(<Content />, document.getElementById('iwgm'));
+  }, []);
+
+  const _createInfoWindow = useCallback(
+    ({
+      content,
+      onDomReady,
+      onCloseClickOnce,
+      onContentChanged,
+      onCloseClick,
+      onPositionChanged,
+      onZIndexChanged,
+    }) => {
+      const configs = {
+        content: `<div id="iwgm"></div>`,
+      };
+
+      const instancedInfoWindow = new window.google.maps.InfoWindow(configs);
+
+      const events = [
+        { name: 'closeclick', callback: onCloseClick, onceCallback: onCloseClickOnce },
+        { name: 'content_changed', callback: onContentChanged },
+        {
+          name: 'domready',
+          callback: () => {
+            _renderToDom(content);
+
+            if (onDomReady && isAFunction(onDomReady)) {
+              onDomReady();
+            }
+          },
+        },
+        { name: 'position_changed', callback: onPositionChanged },
+        { name: 'zindex_changed', callback: onZIndexChanged },
+      ];
+
+      setupGoogleMapsEventListeners(instancedInfoWindow, events);
+
+      return instancedInfoWindow;
+    },
+    []
+  );
+
   const _initMarkers = useCallback(() => {
     _setMarkers(
-      markers.map(
-        (marker) =>
-          new window.google.maps.Marker({
-            map: mapInstance,
-            position: marker.position,
-          })
-      )
+      markers.map((marker) => {
+        const instancedMarker = new window.google.maps.Marker({
+          map: mapInstance,
+          position: marker.position,
+        });
+
+        if (marker.infoWindow && marker.infoWindow.content) {
+          instancedMarker.addListener('click', () => {
+            // Maybe clear opened info windows.
+            const infoWindow = _createInfoWindow(marker.infoWindow);
+
+            infoWindow.open(mapInstance, instancedMarker);
+          });
+        }
+
+        return instancedMarker;
+      })
     );
   }, [mapInstance, markers]);
 
